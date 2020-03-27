492bdf77a428f88c995903407547cef969bbed06
Rheniery <rhenierymendes@gmail.com>
2020-03-27 19:33:51 -0300

    front-end from the app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import './global.css';
+import Routes from './routes';
+//JSX (Javascript XML)
+function App() {
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
